b3f8c7de1bc9f298bece5ef3f6543eece325fd8e
Divyagnan Reddy <divyagnanme@gmail.com>
2024-08-25 19:14:49 +0530

    changed dataset

--- classification_algos/KNN.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import style
-import seaborn as sns
-
-data = pd.read_csv("adult_data.csv", delimiter=", ")
-df = pd.DataFrame(data)
-print(df.head())
