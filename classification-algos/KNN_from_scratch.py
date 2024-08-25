847862a5d732ee48cc8783f6086e6d5fc3a687fd
Divyagnan Reddy <divyagnanme@gmail.com>
2024-08-25 21:57:03 +0530

    KNN from scratch with accuracy

--- classification-algos/KNN_from_scratch.py
+++ classification-algos/KNN_from_scratch.py
@@ -1,44 +1,58 @@
 import numpy as np
-import math
 import pandas as pd
-from matplotlib import style
-
-style.use("fivethirtyeight")
-
-data = pd.read_csv("iris.data")
-df = pd.DataFrame(data)
-df.replace({"Iris-setosa": 1, "Iris-versicolor": 2, "Iris-virginica": 3}, inplace=True)
-X = np.array(df.drop(["class"], axis=1))
-Y = np.array(df["class"])
-
-
-def K_nearest(input_features, X, Y, k=4):
-    # Create a list to store distances and their corresponding classes
-    distances = []
-    for i in range(len(X)):
-        # Calculate the Euclidean distance between input_features and X[i]
-        distance = np.sqrt(np.sum((input_features - X[i]) ** 2))
-        distances.append((distance, Y[i]))
-
-    # Sort the distances and classes by distance
-    sorted_distances = sorted(distances, key=lambda x: x[0])
-
-    # Count the votes for the k nearest neighbors
-    classes = {1: 0, 2: 0, 3: 0}
-    for i in range(k):
-        _, class_label = sorted_distances[i]
-        classes[class_label] += 1
-
-    # Return the class with the maximum votes
-    return max(classes, key=lambda k: classes[k])
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+import math
+import sklearn.neighbors
+df = pd.DataFrame(pd.read_csv("iris.data"))
+df.replace({"Iris-virginica": 3, "Iris-versicolor": 2, "Iris-setosa": 1}, inplace=True)
+X = df.drop(["class"], axis=1)
+Y = df["class"]
+train_x, test_x, train_y, test_y = train_test_split(X, Y, test_size=0.2)
 
 
-prediction = K_nearest([8.7, 3.0, 5.8, 2.1], X, Y, k=4)
-if prediction == 1:
-    print("Iris-setosa")
-elif prediction == 2:
-    print("Iris-versicolor")
-elif prediction == 3:
-    print("Iris-virginica")
+def euclidian_distance(i1, i2):
+    return math.sqrt(sum((i1 - i2) ** 2))
 
-#  note Have to implement the score functinality by applying one more outer for loop for all the given inputs
+
+def KNN(train_x, test_x, train_y, test_y, k=4):
+    predictions = []
+    x = np.array(train_x)
+    y = np.array(train_y)
+    test_x = np.array(test_x)
+    test_y = np.array(test_y)
+    for instance in test_x:
+        votes = {}
+        for i in range(len(train_x)):
+            distance = euclidian_distance(instance, train_x[i])
+            votes[distance] = train_y[i]
+        ascending_votes = sorted(votes.keys())
+        classes = {1: 0, 2: 0, 3: 0}
+        for i in range(k):
+            class_label = votes[ascending_votes[i]]
+            classes[class_label] += 1
+        prediction = max(classes, key=lambda k: classes[k])
+        predictions.append(prediction)
+
+    def calculate_accuracy(pred, y):
+        count = 0
+        for i in range(len(y)):
+            if pred[i] == y[i]:
+                count += 1
+        return (count/len(y))*100
+    acc = calculate_accuracy(predictions, test_y)
+    return acc, predictions
+
+
+train_x = np.array(train_x)
+train_y = np.array(train_y)
+test_x = np.array(test_x)
+test_y = np.array(test_y)
+acc, predictions = KNN(train_x, test_x, train_y, test_y, k=4)
+print(acc)
+for i in range(len(test_y)):
+    print(f"predicted:{predictions[i]}, original: {test_y[i]}")
+
+plt.scatter(predictions, test_y)
+plt.show()
